8274c82a7392f3b2c2808e7d0a869479042d7517
john rey goh <johnreygoh@gmail.com>
2025-02-05 15:38:59 +0800

    fixed secgrp

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "johnec2" {
-  ami             = "ami-0c614dee691cbbf37"
-  instance_type   = "t2.micro"
-  key_name        = "devopskeypair"
-  subnet_id       = aws_subnet.subnet1.id
-  security_groups = [aws_security_group.johnsg.name]
+  ami           = "ami-0c614dee691cbbf37"
+  instance_type = "t2.micro"
+  key_name      = "devopskeypair"
+  subnet_id     = aws_subnet.subnet1.id
+  vpc_security_group_ids = [
+    aws_security_group.johnsg.id
+  ]
 
   tags = {
     Name = "johnec2"
